45701b2c701be25067c81bae372e5c152231f816
Abilasha06 <bosethabilash2003@gmail.com>
2026-04-23 23:10:06 +0530

    edit Student.java file

--- FacultySystem/src/model/Student.java
+++ FacultySystem/src/model/Student.java
@@ -7,7 +7,7 @@ public class Student extends User {
     private String    depId;
     private LocalDate dob;
     private int       age;
-    private String    type; // Proper / Repeat / Suspended
+    private String    type;
 
     public Student(String userId, String regNo, String role, String password,
                    String email, String fullname, String depId, LocalDate dob, int age, String type) {
@@ -25,7 +25,7 @@ public class Student extends User {
     public int       getAge()         { return age; }
     public String    getType()        { return type; }
     public boolean   isRepeat()       { return "Repeat".equals(type); }
-    public boolean   isSuspended()    { return "Suspended".equals(type); }
+    public boolean   isBatchMissed()    { return "BatchMissed".equals(type); }
     public String    getStudentType() { return type != null ? type : "Proper"; }
 
     @Override public void openDashboard() {
